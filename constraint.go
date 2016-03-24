f9e90667c8e3fedb375cc93b123e325668135173
Sam Boyer <tech@samboyer.org>
2016-03-24 17:17:59 -0400

    First pass at new parser

--- constraint.go
+++ constraint.go
@@ -81,25 +81,50 @@ func (none) IsMagic() bool {
 }
 
 type rangeConstraint struct {
-	min, max *constraint
-	excl     []*constraint
+	min, max               *Version
+	includeMin, includeMax bool
+	excl                   []*Version
 }
 
 func (rc rangeConstraint) Admits(v *Version) error {
+	var fail bool
+	var emsg string
 	if rc.min != nil {
-		if !rc.min.check(v) {
-			return fmt.Errorf(rc.min.msg, v, rc.min.orig)
+		// TODO ensure sane handling of prerelease versions (which are strictly
+		// less than the normal version, but should be admitted in a geq range)
+		cmp := rc.min.Compare(v)
+		if rc.includeMin {
+			emsg = "%s is less than %s"
+			fail = cmp == 1
+		} else {
+			emsg = "%s is less than or equal to %s"
+			fail = cmp != -1
+		}
+
+		if fail {
+			return fmt.Errorf(emsg, v, rc.min.String())
 		}
 	}
 
 	if rc.max != nil {
-		if !rc.min.check(v) {
-			return fmt.Errorf(rc.max.msg, v, rc.max.orig)
+		// TODO ensure sane handling of prerelease versions (which are strictly
+		// less than the normal version, but should be admitted in a geq range)
+		cmp := rc.max.Compare(v)
+		if rc.includeMax {
+			emsg = "%s is greater than %s"
+			fail = cmp == -1
+		} else {
+			emsg = "%s is greater than or equal to %s"
+			fail = cmp != 1
+		}
+
+		if fail {
+			return fmt.Errorf(emsg, v, rc.max.String())
 		}
 	}
 
 	for _, excl := range rc.excl {
-		if excl.con.Equal(v) {
+		if excl.Equal(v) {
 			return fmt.Errorf("Version %s is specifically disallowed.", v.String())
 		}
 	}
@@ -116,7 +141,7 @@ func (rc rangeConstraint) Intersect(c Constraint) Constraint {
 	case unionConstraint:
 		return oc.Intersect(rc)
 	case *Version, rangeConstraint:
-		panic("not implemented")
+		break
 	default:
 		// this duplicates what's above, but doing it this way allows a slightly
 		// faster path for internal operations while still respecting the
@@ -130,6 +155,8 @@ func (rc rangeConstraint) Intersect(c Constraint) Constraint {
 		}
 		panic("unknown type")
 	}
+
+	panic("not implemented")
 }
 
 func (rc rangeConstraint) AdmitsAny() bool {
@@ -140,22 +167,56 @@ func (rc rangeConstraint) IsMagic() bool {
 	return false
 }
 
-type unionConstraint struct {
-	constraints []Constraint
-}
+type unionConstraint []Constraint
 
-func (unionConstraint) Admits(v *Version) error {
+func (uc unionConstraint) Admits(v *Version) error {
 	panic("not implemented")
 }
 
-func (unionConstraint) Intersect(Constraint) Constraint {
-	panic("not implemented")
+func (uc unionConstraint) Intersect(Constraint) Constraint {
+	panic("not implemented - this is really the one annoying bit")
 }
 
-func (unionConstraint) AdmitsAny() bool {
+func (uc unionConstraint) AdmitsAny() bool {
 	return true
 }
 
-func (unionConstraint) IsMagic() bool {
+func (uc unionConstraint) IsMagic() bool {
 	return false
+}
+
+// Intersection computes the intersection between N Constraints, returning as
+// compact a representation of the intersection as possible.
+//
+// No error is indicated if all the sets are collectively disjoint; you must inspect the
+// return value to see if the result is the empty set (indicated by both
+// IsMagic() being true, and AdmitsAny() being false).
+func Intersection(cg ...Constraint) Constraint {
+	// If there's zero or one constraints in the group, we can quit fast
+	switch len(cg) {
+	case 0:
+		// Zero members means unconstrained, so return any
+		return any{}
+	case 1:
+		// Just one member means that's our final constraint
+		return cg[0]
+	}
+
+	// Do a preliminary first pass to see if we have any constraints that
+	// supercede everything else, making it easy
+	for _, c := range cg {
+		switch c.(type) {
+		case none, *Version:
+			return c
+		}
+	}
+
+	// Now we know there's no easy wins, so step through and intersect each with
+	// the previous
+	head, tail := cg[0], cg[1:]
+	for _, c := range tail {
+		head = head.Intersect(c)
+	}
+
+	return head
 }
--- constraints.go
+++ constraints.go
@@ -83,6 +83,7 @@ func (cs Constraints) Validate(v *Version) (bool, []error) {
 	return false, e
 }
 
+/*
 func (cs Constraints) Intersect(other ...*Constraints) *Constraints {
 	// TODO not a pointer receiver...just overwrite cs?
 	rc := cs
@@ -102,7 +103,7 @@ func (cs Constraints) Intersect(other ...*Constraints) *Constraints {
 			}
 
 			switch r := c.(type) {
-			case None:
+			case none:
 				// Arriving at 'None' at any point guarantees our final answer
 				// will also be 'None'
 				// TODO ugh clean up how this is done
@@ -131,9 +132,10 @@ func (cs Constraints) Intersect(other ...*Constraints) *Constraints {
 
 	return &rc
 }
-
+*/
 type constraintGroup []*constraint
 
+/*
 func (cg constraintGroup) asConstraint() Constraint {
 	if len(cg) == 0 {
 		return nil
@@ -182,6 +184,7 @@ func (cg constraintGroup) asConstraint() Constraint {
 
 	return rc
 }
+*/
 
 var constraintOps map[string]cfunc
 var constraintMsg map[string]string
@@ -303,6 +306,157 @@ func parseConstraint(c string) (*constraint, error) {
 		dirty:      dirty,
 	}
 	return cs, nil
+}
+
+func parseConstraintNu(c string) (Constraint, error) {
+	m := constraintRegex.FindStringSubmatch(c)
+	if m == nil {
+		return nil, fmt.Errorf("Malformed constraint: %s", c)
+	}
+
+	// Handle the full wildcard case first - easy!
+	if isX(m[3]) {
+		return any{}, nil
+	}
+
+	ver := m[2]
+	var wildPatch, wildMinor bool
+	if isX(strings.TrimPrefix(m[4], ".")) {
+		wildPatch = true
+		wildMinor = true
+		ver = fmt.Sprintf("%s.0.0%s", m[3], m[6])
+	} else if isX(strings.TrimPrefix(m[5], ".")) {
+		wildPatch = true
+		ver = fmt.Sprintf("%s%s.0%s", m[3], m[4], m[6])
+	}
+
+	v, err := NewVersion(ver)
+	if err != nil {
+		// The constraintRegex should catch any regex parsing errors. So,
+		// we should never get here.
+		return nil, errors.New("constraint Parser Error")
+	}
+
+	switch m[1] {
+	case "^":
+		// Caret always expands to a range
+		return expandCaret(v), nil
+	case "~":
+		// Tilde always expands to a range
+		return expandTilde(v, wildMinor), nil
+	case "!=":
+		// Not equals only expands to a range if any element isX()
+		return expandNeq(v, wildMinor, wildPatch), nil
+	case "", "=":
+		if wildPatch || wildMinor {
+			// Equalling a wildcard has the same behavior as expanding tilde
+			return expandTilde(v, wildMinor), nil
+		}
+		return v, nil
+	case ">":
+		return expandGreater(v, false), nil
+	case ">=", "=>":
+		return expandGreater(v, true), nil
+	case "<":
+		return expandLess(v, false), nil
+	case "<=", "=<":
+		return expandLess(v, true), nil
+	default:
+		// Shouldn't be possible to get here, unless the regex is allowing
+		// predicate we don't know about...
+		return nil, fmt.Errorf("Unrecognized predicate %q", m[1])
+	}
+}
+
+func expandCaret(v *Version) Constraint {
+	maxv := &Version{
+		major: v.major + 1,
+		minor: 0,
+		patch: 0,
+	}
+
+	return rangeConstraint{
+		min:        v,
+		max:        maxv,
+		includeMin: true,
+		includeMax: false,
+	}
+}
+
+func expandTilde(v *Version, wildMinor bool) Constraint {
+	if wildMinor {
+		// When minor is wild on a tilde, behavior is same as caret
+		return expandCaret(v)
+	}
+
+	maxv := &Version{
+		major: v.major,
+		minor: v.minor + 1,
+		patch: 0,
+	}
+
+	return rangeConstraint{
+		min:        v,
+		max:        maxv,
+		includeMin: true,
+		includeMax: false,
+	}
+}
+
+// expandNeq expands a "not-equals" constraint.
+//
+// If the constraint has any wildcards, it will expand into a unionConstraint
+// (which is how we represent a disjoint set). If there are no wildcards, it
+// will expand to a rangeConstraint with no min or max, but having the one
+// exception.
+func expandNeq(v *Version, wildMinor, wildPatch bool) Constraint {
+	if !(wildMinor || wildPatch) {
+		return rangeConstraint{
+			excl: []*Version{v},
+		}
+	}
+
+	// Create the low range with no min, and the max as the floor admitted by
+	// the wildcard
+	lr := rangeConstraint{
+		max:        v,
+		includeMax: false,
+	}
+
+	// The high range uses the derived version, bumped depending on where the
+	// wildcards where, as the min, and is inclusive
+	minv := &Version{
+		major: v.major,
+		minor: v.minor,
+		patch: v.patch,
+	}
+
+	if wildMinor {
+		minv.major++
+	} else {
+		minv.minor++
+	}
+
+	hr := rangeConstraint{
+		min:        minv,
+		includeMin: true,
+	}
+
+	return unionConstraint{lr, hr}
+}
+
+func expandGreater(v *Version, eq bool) Constraint {
+	return rangeConstraint{
+		min:        v,
+		includeMin: eq,
+	}
+}
+
+func expandLess(v *Version, eq bool) Constraint {
+	return rangeConstraint{
+		max:        v,
+		includeMax: eq,
+	}
 }
 
 // Constraint functions
