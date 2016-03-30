59762782ee93b06c0e4c54297b95e02b096edb7d
Sam Boyer <tech@samboyer.org>
2016-03-29 20:06:22 -0400

    Add basic union and intersection benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -8,6 +8,41 @@ func init() {
 	cacheVersions = false
 }
 
+var (
+	rc1 = rangeConstraint{
+		min:        newV(1, 5, 0),
+		max:        newV(2, 0, 0),
+		includeMax: true,
+	}
+	rc2 = rangeConstraint{
+		min: newV(2, 0, 0),
+		max: newV(3, 0, 0),
+	}
+	rc3 = rangeConstraint{
+		min: newV(1, 5, 0),
+		max: newV(2, 0, 0),
+	}
+	rc4 = rangeConstraint{
+		min: newV(1, 7, 0),
+		max: newV(4, 0, 0),
+	}
+	rc5 = rangeConstraint{
+		min: newV(2, 7, 0),
+		max: newV(3, 0, 0),
+	}
+	rc6 = rangeConstraint{
+		min: newV(3, 0, 1),
+		max: newV(3, 0, 4),
+	}
+	rc7 = rangeConstraint{
+		min: newV(1, 0, 0),
+		max: newV(1, 2, 0),
+	}
+	// Two fully non-overlapping unions
+	u1 = rc1.Union(rc7)
+	u2 = rc5.Union(rc6)
+)
+
 /* Constraint creation benchmarks */
 
 func benchNewConstraint(c string, b *testing.B) {
@@ -121,4 +156,104 @@ func BenchmarkNewVersionMeta(b *testing.B) {
 
 func BenchmarkNewVersionMetaDash(b *testing.B) {
 	benchNewVersion("1.0.0+metadata-dash", b)
+}
+
+/* Union benchmarks */
+
+func BenchmarkAdjacentRangeUnion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Union(rc1, rc2)
+	}
+}
+
+func BenchmarkAdjacentRangeUnionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc1.Union(rc2)
+	}
+}
+
+func BenchmarkDisjointRangeUnion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Union(rc2, rc3)
+	}
+}
+
+func BenchmarkDisjointRangeUnionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc2.Union(rc3)
+	}
+}
+
+func BenchmarkOverlappingRangeUnion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Union(rc1, rc4)
+	}
+}
+
+func BenchmarkOverlappingRangeUnionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc1.Union(rc4)
+	}
+}
+
+func BenchmarkUnionUnion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Union(u1, u2)
+	}
+}
+
+func BenchmarkUnionUnionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		u1.Union(u2)
+	}
+}
+
+/* Intersection benchmarks */
+
+func BenchmarkSubsetRangeIntersection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Intersection(rc2, rc4)
+	}
+}
+
+func BenchmarkSubsetRangeIntersectionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc2.Intersect(rc4)
+	}
+}
+
+func BenchmarkDisjointRangeIntersection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Intersection(rc2, rc3)
+	}
+}
+
+func BenchmarkDisjointRangeIntersectionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc2.Intersect(rc3)
+	}
+}
+
+func BenchmarkOverlappingRangeIntersection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Intersection(rc1, rc4)
+	}
+}
+
+func BenchmarkOverlappingRangeIntersectionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rc1.Intersect(rc4)
+	}
+}
+
+func BenchmarkUnionIntersection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Intersection(u1, u2)
+	}
+}
+
+func BenchmarkUnionIntersectionMethod(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		u1.Intersect(u2)
+	}
 }
