bc1329a1a3ec1866d25f459f4cb37cb42fbb8e98
sam boyer <tech@samboyer.org>
2016-11-28 10:26:52 -0500

    Put zero/inf checks behind a method call
    Standard argument for this - it makes it easier to refactor the
    implementation later.

--- constraints_test.go
+++ constraints_test.go
@@ -92,19 +92,19 @@ func constraintEq(c1, c2 Constraint) bool {
 				return false
 			}
 
-			if tc1.min != nil {
+			if !tc1.hasZeroMin() {
 				if !(tc1.includeMin == tc2.includeMin && tc1.min.Equal(tc2.min)) {
 					return false
 				}
-			} else if tc2.min != nil {
+			} else if !tc2.hasZeroMin() {
 				return false
 			}
 
-			if tc1.max != nil {
+			if !tc1.hasInfMax() {
 				if !(tc1.includeMax == tc2.includeMax && tc1.max.Equal(tc2.max)) {
 					return false
 				}
-			} else if tc2.max != nil {
+			} else if !tc2.hasInfMax() {
 				return false
 			}
 
